db72290c9f1a9d7ec1b135fdcb7dd135c1dd649a
Falseness <baharevuniversal@gmail.com>
2021-04-14 03:31:05 +0300

    pygame template added

--- terrain_generation/main.py
+++ terrain_generation/main.py
@@ -1,5 +1,39 @@
+import pygame
+import sys
+import math
+from math import sqrt
+from random import randrange
+
+width = 800
+height = 600
+size = [width, height]
+black = [0, 0, 0]
+green = [0, 255, 0]
+
 def main():
-    print('Hello world!')
+    pygame.init()                               # иницилизация библиотеки
+    pygame.font.init()
+
+    screen = pygame.display.set_mode(size)      # создание окна и установка размера
+    game_over = False
+
+    font = pygame.font.SysFont('Comic Sans MS', 30, True, False)
+
+    text_x = 100
+    text_y = 100
+
+
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+
+        screen.fill(black)
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+    sys.exit()
+
 
 if __name__ == '__main__':
     main()
