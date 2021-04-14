3789d2bd40eb2d6c28317459b8b3ffd391adf5ea
Falseness <baharevuniversal@gmail.com>
2021-04-14 22:06:18 +0300

    simple generation outline

--- terrain_generation/main.py
+++ terrain_generation/main.py
@@ -1,27 +1,69 @@
 import pygame
 import sys
-import math
-from math import sqrt
-from random import randrange
+from time import time
+from perlin_noise import PerlinNoise
 
-width = 800
-height = 600
-size = [width, height]
+
+size = [200, 200]
 black = [0, 0, 0]
 green = [0, 255, 0]
 
+class GameOptions:
+    WIDTH = 400
+    HEIGHT = 400
+    SIZE = [WIDTH, HEIGHT]
+
+class MapData:
+    def __init__(self, height: int, width: int):
+        self._matrix = [[0.0 for x in range(width)] for y in range(height)]
+
+class Generator:
+    def __init__(self):
+        pass
+
+
+
 def main():
-    pygame.init()                               # иницилизация библиотеки
+    tmp = round(time() * 1000)
+    print(tmp)
+    noise = PerlinNoise(octaves=3, seed=tmp)
+    xpix, ypix = GameOptions.HEIGHT, GameOptions.WIDTH
+    pic = [[noise([i / xpix, j / ypix]) for j in range(xpix)] for i in range(ypix)]
+    '''for q in range(4):
+        noise2 = PerlinNoise(octaves=4, seed=(q + 2)*tmp)
+        pic2 = [[noise2([i / xpix, j / ypix]) for j in range(xpix)] for i in range(ypix)]
+        for i in range(len(pic)):
+            for j in range(len(pic[i])):
+                pic[i][j] += pic2[i][j]
+                '''
+    maximum_value = pic[0][0]
+    minimum_value = pic[0][0]
+    for i in range(len(pic)):
+        for j in range(len(pic[i])):
+            maximum_value = max(maximum_value, pic[i][j])
+            minimum_value = min(minimum_value, pic[i][j])
+    for i in range(len(pic)):
+        for j in range(len(pic[i])):
+            pic[i][j] = (pic[i][j] - minimum_value) / (maximum_value - minimum_value)
+    pygame.init()
     pygame.font.init()
 
-    screen = pygame.display.set_mode(size)      # создание окна и установка размера
+    screen = pygame.display.set_mode(GameOptions.SIZE)
     game_over = False
 
-    font = pygame.font.SysFont('Comic Sans MS', 30, True, False)
-
-    text_x = 100
-    text_y = 100
-
+    sur = pygame.Surface(GameOptions.SIZE)
+    for i in range(len(pic)):
+        for j in range(len(pic[i])):
+            color_value = 255 * pic[i][j]
+            if color_value < 70:
+                color = [0, 0, 100]
+            elif color_value < 140:
+                color = [0, 0, 200]
+            elif color_value < 180:
+                color = [150, 150, 0]
+            else:
+                color = [0, 200, 0]
+            sur.set_at((i, j), color)
 
     while not game_over:
         for event in pygame.event.get():
@@ -30,6 +72,7 @@ def main():
 
         screen.fill(black)
 
+        screen.blit(sur, (0, 0))
         pygame.display.flip()
         pygame.time.wait(10)
     sys.exit()
